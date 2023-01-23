f400d8a3e1159ebc16a86f4294bbae38a815746e
Gus Oliver <gustavooliver.souza@gmail.com>
2023-01-22 21:55:29 -0300

    player-interaction

--- game.js
+++ game.js
@@ -1,3 +1,18 @@
-let board = ["","","", "","","", "","",""];
+let board = ["", "", "", "", "", "", "", "", ""];
 let player = 0;
-let symbol = "x"
+let symbol = ["x", "o"]
+
+function turnAction(pos) {
+
+    if (board[pos] == "") {
+        
+        board[pos] = symbol[player];
+
+        if (player == 0) {
+            player = 1;
+        }
+        else {
+            player = 0;
+        }
+    }
+}
--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 let fields = document.querySelectorAll(".fields");
 
-fields.forEach((square=>{
+fields.forEach((square => {
     square.addEventListener("click", action);
 }))
 
-function action(){}
+function action(e) {
+    console.log(e.target);
+
+    let square = e.target;
+    let pos = square.id;
+
+    turnAction(pos);
+    updateBoard();
+}
+
+function updateBoard() {
+    let fields = document.querySelectorAll(".fields");
+
+    fields.forEach((square => {
+        let pos = square.id;
+        let sym = board[pos];
+
+        if(sym != ""){
+            square.innerHTML = "<div class="+sym+"></div>"
+        }
+    }))
+    }
