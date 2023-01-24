d650db09f5592443b3049882aace98e3e99e1e51
Gus Oliver <gustavooliver.souza@gmail.com>
2023-01-23 22:21:10 -0300

    game-js

--- game.js
+++ game.js
@@ -1,18 +1,55 @@
-let board = ["", "", "", "", "", "", "", "", ""];
+/* VARIABLES */
+let board = ["", "", "", "", "", "", "", "", "",];
 let player = 0;
-let symbol = ["x", "o"]
+let symbol = ["x", "o"];
+let winCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let gameOver = false;
 
-function turnAction(pos) {
+
+function turnAction(pos) { /* Players Move and Win Check */
+    if (gameOver == true) {
+        return;
+    }
 
     if (board[pos] == "") {
-        
         board[pos] = symbol[player];
+        gameOver = win();
 
-        if (player == 0) {
-            player = 1;
+        if (gameOver == false) {
+            if (player == 0) {
+                player = 1;
+            }
+            else {
+                player = 0;
+            }
         }
-        else {
-            player = 0;
+        return gameOver;
+    }
+}
+
+function win() { /* Check win and Endgame */
+
+    for (i = 0; i < winCondition.length; i++) {
+        let seq = winCondition[i];  /* sequence */
+
+        pos1 = seq[0];  /* position */
+        pos2 = seq[1];
+        pos3 = seq[2];
+
+        if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != "") {
+            gameStop();
+            return true;
         }
     }
+
+    return false;
 }
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let fields = document.querySelectorAll(".fields");
+let root = document.querySelector(":root");
 
 fields.forEach((square => {
     square.addEventListener("click", action);
 }))
 
 function action(e) {
-    console.log(e.target);
 
     let square = e.target;
-    let pos = square.id;
+    let pos = square.id;  /* position */
 
     turnAction(pos);
     updateBoard();
@@ -21,8 +21,11 @@ function updateBoard() {
         let pos = square.id;
         let sym = board[pos];
 
-        if(sym != ""){
-            square.innerHTML = "<div class="+sym+"></div>"
+        if (sym != "") {
+            square.innerHTML = "<div class=" + sym + "></div>";
         }
     }))
-    }
+}
+function gameStop(){
+    root.style = "--color1: rgb(166, 150, 3); --color2: rgb(207, 194, 7); --color3: rgb(003, 074, 166); --color4:rgba(4, 132, 217, 0.521);"
+}
